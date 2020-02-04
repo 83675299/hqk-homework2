423445c6e8626b78837c76b5f6ae4fe36d02f9ee
hqk111 <83675299@qq.com>
2020-02-04 19:54:23 +0800

    Revert "第二次寒假作业"
    This reverts commit 62744c20f7a01fbf675d14e611ed4d233d7bf49e.

--- 1.cmd
+++
@@ -1,9 +0,0 @@
-@pause
-@echo off
-set /p var=请输入文件所在
-cd "%var%"
-set /p var=请输入文件名
-gcc "%var%" -o lang.exe
-if exist "lang.exe" echo 编译成功
-if not exist "lang.exe" echo 编译失败
-pause
--- 2.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-echo ¿ªÊ¼²âÊÔ
-test1.exe
-echo ²âÊÔ½áÊø
-pause
